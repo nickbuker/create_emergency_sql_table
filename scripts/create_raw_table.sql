9fd2d03ee6f4a8bb5271f1fb3bb48bce1efd859b
nickbuker <nickbuker@gmail.com>
2017-02-07 10:08:20 -0800

    add raw table scripts

---
+++ scripts/create_raw_table.sql
@@ -0,0 +1,38 @@
+CREATE TABLE
+  raw_emergency
+  (id_num integer
+  ,Address text
+  ,Type text
+  ,Datetime timestamp
+  ,Latitude decimal
+  ,Longitude decimal
+  ,"Report Location" text
+  ,"Incident Number" text
+  ,dt_crop text
+  ,date date
+  ,time text
+  ,date2 timestamp
+  ,time2 text
+  ,mariners_home integer
+  ,seahawks_home integer
+  ,sounders_home integer
+  ,trav_holiday integer
+  ,dang_holiday integer
+  ,weekday text
+  ,night integer
+  ,Monday integer
+  ,Saturday integer
+  ,Sunday integer
+  ,Thursday integer
+  ,Tuesday integer
+  ,Wednesday integer
+  ,day_num integer
+  ,neighborhood text
+  ,zone1 integer
+  ,zone2 integer
+  ,zone3 integer
+  ,zone4 integer
+  ,zone5 integer
+  ,zone6 integer
+  ,zone7 integer
+  ,seasonality decimal);
---
+++ scripts/populate_raw_table.sql
@@ -0,0 +1,86 @@
+COPY
+  raw_emergency
+  (id_num
+  ,Address
+  ,Type
+  ,Datetime
+  ,Latitude
+  ,Longitude
+  ,"Report Location"
+  ,"Incident Number"
+  ,dt_crop
+  ,date
+  ,time
+  ,date2
+  ,time2
+  ,mariners_home
+  ,seahawks_home
+  ,sounders_home
+  ,trav_holiday
+  ,dang_holiday
+  ,weekday
+  ,night
+  ,Monday
+  ,Saturday
+  ,Sunday
+  ,Thursday
+  ,Tuesday
+  ,Wednesday
+  ,day_num
+  ,neighborhood
+  ,zone1
+  ,zone2
+  ,zone3
+  ,zone4
+  ,zone5
+  ,zone6
+  ,zone7
+  ,seasonality)
+FROM
+  '/home/nick/Desktop/projects/seattle_emergency_database/data/seattle_911_prepped_no_out1.csv'
+  HEADER
+  CSV;
+
+
+COPY
+  raw_emergency
+  (id_num
+  ,Address
+  ,Type
+  ,Datetime
+  ,Latitude
+  ,Longitude
+  ,"Report Location"
+  ,"Incident Number"
+  ,dt_crop
+  ,date
+  ,time
+  ,date2
+  ,time2
+  ,mariners_home
+  ,seahawks_home
+  ,sounders_home
+  ,trav_holiday
+  ,dang_holiday
+  ,weekday
+  ,night
+  ,Monday
+  ,Saturday
+  ,Sunday
+  ,Thursday
+  ,Tuesday
+  ,Wednesday
+  ,day_num
+  ,neighborhood
+  ,zone1
+  ,zone2
+  ,zone3
+  ,zone4
+  ,zone5
+  ,zone6
+  ,zone7
+  ,seasonality)
+FROM
+  '/home/nick/Desktop/projects/seattle_emergency_database/data/seattle_911_prepped_no_out2.csv'
+  HEADER
+  CSV;
